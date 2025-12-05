52b82150b5d59590286128726969edc5084605af
Ban B. Wong <healthyqi@yahoo.com>
2025-12-04 19:03:50 -0700

    Update import path for STIDACalculator component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import STIDACalculator from "./STIDACalculator";
+import STIDACalculator from "./components/STIDACalculator";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
